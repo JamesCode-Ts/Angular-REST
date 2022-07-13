efe4759f62a1e69e95218067b7ff0a79de974ba9
JamesCode-Ts@github.com <jameslob42@gmail.com>
2022-07-13 19:24:51 -0300

    Class Model e Service do Usuário

--- Curso-Angular-REST/src/app/service/header-interceptor.service.ts
+++ Curso-Angular-REST/src/app/service/header-interceptor.service.ts
@@ -7,23 +7,23 @@ import { Observable } from 'rxjs';
 })
 export class HeaderInterceptorService implements HttpInterceptor{
 
-  
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        throw new Error('Method not implemented.');
-    
-    if (localStorage.getItem('token') !== null) {   /*Verifica se existe um token no localStore e se ele for diferente de nulo, ou seja, se ele existir.  */
-      const token = 'Bearer ' + localStorage.getItem('token');   /* pega o token e passa para a costante, respeitando  o prefixo bearer e o tokem propriamente dito. */
 
-      const tokenRequest = req.clone({  /*Agora faz um clone  e seta a autorização e o token pelo o cabeçalho */
-        headers: req.headers.set('Authorization', token)
-      });
+	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		throw new Error('Method not implemented.');
 
-      return next.handle(tokenRequest); /*No final retorna a requisição do token para o back-end */
-    } else {
-      return next.handle(req); /** Caso não for achado o token no localstorage ou seja no navegador, faz uma requisição */
-    }
+		if (localStorage.getItem('token') !== null) {   /*Verifica se existe um token no localStore e se ele for diferente de nulo, ou seja, se ele existir.  */
+			const token = 'Bearer ' + localStorage.getItem('token');   /* pega o token e passa para a costante, respeitando  o prefixo bearer e o tokem propriamente dito. */
 
-  }
+			const tokenRequest = req.clone({  /*Agora faz um clone  e seta a autorização e o token pelo o cabeçalho */
+				headers: req.headers.set('Authorization', token)
+			});
+
+			return next.handle(tokenRequest); /*No final retorna a requisição do token para o back-end */
+		} else {
+			return next.handle(req); /** Caso não for achado o token no localstorage ou seja no navegador, faz uma requisição */
+		}
+
+	}
     constructor() { }
 }
 
